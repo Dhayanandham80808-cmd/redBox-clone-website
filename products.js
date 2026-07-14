78ab48b9a0142150b3292af8e8408f850ae7fc26
Dhayanandham80808-cmd <dhayanandham80808@gmail.com>
2026-07-14 15:51:55 +0530

    updated my project

---
+++ products.js
@@ -0,0 +1,38 @@
+export const products = [
+    {
+        name: "Minimal Brown Shirt",
+        price: 999,
+        src: "Minimal Brown Shirt – Clean Casual Style for Men.jpeg",
+        tags: "summer,new"
+    },
+    {
+        name: "Premium Black ",
+        price: 1299,
+        src: "Men's Premium Black Textured Shirt & White Pants Outfit _ Clean Casual Style.jpeg",
+        tags: "party,black,new"
+    },
+    {
+        name: "Black Textured Polo",
+        price: 799,
+        src: "9 Fall Trends You Can Buy Right Now From Amazon—You're Welcome.jpeg",
+        tags: "black,summer"
+    },
+    {
+        name: "Black Leather Jacket",
+        price: 2499,
+        src: "Black leather jacket.jpeg",
+        tags: "black,party,old"
+    },
+    {
+        name: "Casual Outfit",
+        price: 899,
+        src: "Outfit Link 🔗 ⬇️⬇️⬇️🔗.jpeg",
+        tags: "summer,new"
+    },
+    {
+        name: "Relaxed Fit Outfit",
+        price: 999,
+        src: "Relaxed outfit with relaxed pose.jpeg",
+        tags: "summer,old"
+    }
+];
